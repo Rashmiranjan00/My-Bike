c6deb83cdbeab80875b24a77d8a52f8352e7bab7
Rashmiranjan00 <rr.pradhan00@gmail.com>
2019-03-15 21:12:12 +0530

    Weather feature added

--- app/src/main/java/com/zerobug/unite/mybike/DashboardFragment.java
+++ app/src/main/java/com/zerobug/unite/mybike/DashboardFragment.java
@@ -1,34 +1,60 @@
 package com.zerobug.unite.mybike;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.SingleValueDataSet;
-import com.anychart.charts.LinearGauge;
-import com.anychart.enums.Anchor;
-import com.anychart.enums.Orientation;
-import com.anychart.enums.Position;
-import com.anychart.scales.Base;
-import com.anychart.scales.Linear;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.github.anastr.speedviewlib.SpeedView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class DashboardFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class DashboardFragment extends Fragment implements LocationListener {
 
     private View mDashboardView;
     private SpeedView speedoMeter;
-    private AnyChartView anyChartView;
 
     private DatabaseReference mDatabase;
+
+    private String baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private String API = "&APPID=ce0561bfb7f85db1240dbe34ead1561d";
+    private LocationManager locationManager;
+    private String provider;
+    private Location location;
+
+    double lat, lng;
+    String city, myURL;
+
+    private TextView temp, wType, wCity;
 
     public DashboardFragment() {
 
@@ -40,95 +66,161 @@ public class DashboardFragment extends Fragment {
 
         mDashboardView = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        temp = mDashboardView.findViewById(R.id.temp);
+        wType = mDashboardView.findViewById(R.id.wType);
+        wCity = mDashboardView.findViewById(R.id.city);
+
 //        mDatabase = FirebaseDatabase.getInstance().getReference("test");
 //        mDatabase.setValue("Rashmiranjan");
 
         speedoMeter = mDashboardView.findViewById(R.id.speedView);
         speedoMeter.speedTo(50, 4000);
 
-        anyChartView = mDashboardView.findViewById(R.id.any_chart_view);
-        anyChartView.setProgressBar(mDashboardView.findViewById(R.id.progress_bar));
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
-        LinearGauge linearGauge = AnyChart.linear();
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
 
-        // TODO data
-        linearGauge.data(new SingleValueDataSet(new Integer[] { 2 }));
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        linearGauge.tooltip()
-                .useHtml(true)
-                .format(
-                        "function () {\n" +
-                                "          return this.value + '&deg;' + 'C' +\n" +
-                                "            ' (' + (this.value * 1.8 + 32).toFixed(1) +\n" +
-                                "            '&deg;' + 'F' + ')'\n" +
-                                "    }");
+        }
+        location = locationManager.getLastKnownLocation(provider);
 
-        linearGauge.label(0).useHtml(true);
-        linearGauge.label(0)
-                .text("C&deg;")
-                .position(Position.LEFT_BOTTOM)
-                .anchor(Anchor.LEFT_BOTTOM)
-                .offsetY("18px")
-                .offsetX("36%")
-                .fontColor("black")
-                .fontSize(15);
-
-        linearGauge.label(1)
-                .useHtml(true)
-                .text("F&deg;")
-                .position(Position.RIGHT_BOTTOM)
-                .anchor(Anchor.RIGHT_BOTTOM)
-                .offsetY("18px")
-                .offsetX("45%")
-                .fontColor("black")
-                .fontSize(15);
-
-        Base scale = linearGauge.scale()
-                .minimum(-30)
-                .maximum(40);
-//                .setTicks
-
-        linearGauge.axis(0).scale(scale);
-        linearGauge.axis(0)
-                .offset("-1%")
-                .width("0.5%");
-
-        linearGauge.axis(0).labels()
-                .format("{%Value}&deg;")
-                .useHtml(true);
-
-        linearGauge.thermometer(0)
-                .name("Thermometer")
-                .id(1);
-
-        linearGauge.axis(0).minorTicks(true);
-        linearGauge.axis(0).labels()
-                .format(
-                        "function () {\n" +
-                                "    return '<span style=\"color:black;\">' + this.value + '&deg;</span>'\n" +
-                                "  }")
-                .useHtml(true);
-
-        linearGauge.axis(1).minorTicks(true);
-        linearGauge.axis(1).labels()
-                .format(
-                        "function () {\n" +
-                                "    return '<span style=\"color:black;\">' + this.value + '&deg;</span>'\n" +
-                                "  }")
-                .useHtml(true);
-        linearGauge.axis(1)
-                .offset("3.5%")
-                .orientation(Orientation.RIGHT);
-
-        Linear linear = Linear.instantiate();
-        linear.minimum(-20)
-                .maximum(100);
-//                .setTicks
-        linearGauge.axis(1).scale(linear);
-
-        anyChartView.setChart(linearGauge);
+        if (location != null) {
+            Log.d("Provider :", provider + " has been selected.");
+            getLatLng(location);
+            getLocation(location);
+        } else {
+            Log.d("Provider :", "No provider");
+        }
+        
+        getWeather();
 
         return mDashboardView;
+    }
+
+    private void getWeather() {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, myURL, null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("JSON", "JSON: " + response);
+
+                        try {
+                            String info = response.getString("weather");
+                            Log.i("INFO", "INFO: " + info);
+                            JSONArray ar = new JSONArray(info);
+
+                            for (int i = 0; i < ar.length(); i++) {
+                                JSONObject parObj = ar.getJSONObject(i);
+
+                                String myWeather = parObj.getString("main");
+//                                result.setText(myWeather);
+                                Log.d("myweather", myWeather);
+                                Log.d("ID", "ID: " + parObj.getString("id"));
+                                Log.d("MAIN", "MAIN: " + parObj.getString("main"));
+                                wType.setText(myWeather);
+
+                            }
+
+                            JSONObject main = response.getJSONObject("main");
+                            String temperature =  String.format("%.2f", main.getDouble("temp"));
+                            double tempCelcius = Double.parseDouble(temperature);
+                            tempCelcius = tempCelcius - 273.15;
+                            int r = (int) Math.round(tempCelcius*100);
+                            double f = r / 100.0;
+                            temp.setText(f + " ℃");
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("ERROR", "Something Went Wrong " + error);
+
+                    }
+                }
+
+        );
+        MySingleton.getInstance(getActivity()).addToRequestQue(jsonObjectRequest);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    public void getLatLng(Location location) {
+
+        //You had this as int. It is advised to have Lat/Loing as double.
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+
+        Log.d("Latitude: ", String.valueOf(lat));
+        Log.d("Longitude: ", String.valueOf(lng));
+
+    }
+
+    public void getLocation(Location location) {
+
+        try {
+            Geocoder geocoder = new Geocoder(getActivity());
+            List<Address> addresses = null;
+            addresses = geocoder.getFromLocation(lat, lng, 1);
+            city = addresses.get(0).getLocality();
+            Log.d("CITY: ", city);
+            myURL = baseURL + city + API;
+            wCity.setText(city);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(getContext(), "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(getContext(), "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 
 
--- app/src/main/java/com/zerobug/unite/mybike/MainActivity.java
+++ app/src/main/java/com/zerobug/unite/mybike/MainActivity.java
@@ -1,22 +1,38 @@
 package com.zerobug.unite.mybike;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        FirebaseApp.initializeApp(this);
 
 //        Intent loginIntent = new Intent(this, LoginActivity.class);
 //        startActivity(loginIntent);
@@ -31,4 +47,5 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
 
     }
+
 }
