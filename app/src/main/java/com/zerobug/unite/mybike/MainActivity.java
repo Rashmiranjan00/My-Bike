7b5bc6e9c5080b152d4c005a8486e58f498bf703
Rashmiranjan00 <rr.pradhan00@gmail.com>
2019-03-22 23:23:55 +0530

    Warning music added

--- app/src/main/java/com/zerobug/unite/mybike/MainActivity.java
+++ app/src/main/java/com/zerobug/unite/mybike/MainActivity.java
@@ -2,20 +2,22 @@ package com.zerobug.unite.mybike;
 
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,13 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView mNavView;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
+
+    private String playData;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference();
+
         dashboardFragment = new DashboardFragment();
         profileFragment = new ProfileFragment();
         contactFragment = new ContactFragment();
@@ -74,6 +83,31 @@ public class MainActivity extends AppCompatActivity {
             });
 
         }
+
+        playMusic();
+
+    }
+
+    private void playMusic() {
+
+        mRef.child("noti").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                playData = dataSnapshot.getValue().toString();
+                Log.d("PlayMusic", playData);
+                if (!playData.equals("null")) {
+                    int myMusic = getResources().getIdentifier(playData, "raw", "com.zerobug.unite.mybike");
+
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), myMusic);
+                    mediaPlayer.start();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
